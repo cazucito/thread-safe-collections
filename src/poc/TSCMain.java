440f4dc9aedb58f0010f9514630dafb3a03a16a3
Pedro Cazu <dev@cazucito.com>
2018-03-27 03:21:02 -0600

    Adición de soporte a nuevos ejemplos

--- src/poc/TSCMain.java
+++ src/poc/TSCMain.java
@@ -3,6 +3,7 @@
  */
 package poc;
 
+import poc.cc.ConcurrentSkipListMapDemo;
 import poc.cc.ConcurrentSkipListSetDemo;
 import poc.cc.CopyOnWriteArrayListDemo;
 import poc.cc.CopyOnWriteArraySetDemo;
@@ -18,15 +19,17 @@ public class TSCMain {
 	 */
 	public static void main(String[] args) {
 		// CopyOnWriteArrayList
-		CopyOnWriteArrayListDemo.testConstructores();
-		CopyOnWriteArrayListDemo.testCopyOnWriteArrayList();
-		// CopyOnWriteArraySet
-		CopyOnWriteArraySetDemo.testConstructores();
-		CopyOnWriteArraySetDemo.testCopyOnWriteArraySet();
-		// ConcurrentSkipListSet
-		ConcurrentSkipListSetDemo.testConstructores();
-		ConcurrentSkipListSetDemo.testConcurrentSkipListSet();
-
+//		CopyOnWriteArrayListDemo.testConstructores();
+//		CopyOnWriteArrayListDemo.testCopyOnWriteArrayList();
+//		// CopyOnWriteArraySet
+//		CopyOnWriteArraySetDemo.testConstructores();
+//		CopyOnWriteArraySetDemo.testCopyOnWriteArraySet();
+//		// ConcurrentSkipListSet
+//		ConcurrentSkipListSetDemo.testConstructores();
+//		ConcurrentSkipListSetDemo.testConcurrentSkipListSet();
+		// ConcurrentSkipListMapDemo
+		ConcurrentSkipListMapDemo.testConstructores();
+		ConcurrentSkipListMapDemo.testConcurrentSkipListMap();
 	}
 
 }
