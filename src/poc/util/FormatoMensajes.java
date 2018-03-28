c241953a8ce9451f74eddceebc82475ab1562ca4
Pedro Cazu <dev@cazucito.com>
2018-03-28 02:32:07 -0600

    Tipos de mensajes

---
+++ src/poc/util/FormatoMensajes.java
@@ -0,0 +1,9 @@
+package poc.util;
+/**
+ * Tipos de mensaje
+ * @author cazucito
+ *
+ */
+public enum FormatoMensajes {
+	ENCABEZADO, PIE, TITULO, SUBTITULO, MENSAJE, MENSAJE_OK, ERROR_LOGICO, INFO, EXCEPCION, ERROR, DEPURACION
+}
